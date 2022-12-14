2f0c06467c2e8bcf6b4326e7a9e80f2cda160858
Ted Bowman <ted+git@tedbow.com>
2022-12-14 14:04:02 -0500

    add default config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -4,3 +4,5 @@
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
 }
+$config['automatic_updates.settings']['allow_core_minor_updates'] = TRUE;
+$config['project_browser.admin_settings']['allow_ui_install'] = TRUE;
